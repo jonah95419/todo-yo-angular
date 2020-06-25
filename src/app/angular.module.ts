3e3a77474137ca5eb5a1d082ca747eb743e7a3d5
jonah95419 <jonah95419@gmail.com>
2020-06-24 21:54:22 -0500

    MATERIAL ANGULAR MODULE

---
+++ src/app/angular.module.ts
@@ -0,0 +1,61 @@
+import { NgModule } from '@angular/core';
+import { MatTableModule} from '@angular/material/table';
+import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule} from '@angular/material/icon';
+import { MatTooltipModule} from '@angular/material/tooltip';
+import { MatCardModule} from '@angular/material/card';
+import { MatToolbarModule } from  '@angular/material/toolbar';
+import { MatSidenavModule } from  '@angular/material/sidenav';
+import { MatListModule } from  '@angular/material/list';
+import { MatBadgeModule} from '@angular/material/badge';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatSelectModule} from '@angular/material/select';
+import {MatStepperModule} from '@angular/material/stepper';
+import { MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTabsModule } from '@angular/material/tabs';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatRadioModule} from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatRippleModule} from '@angular/material/core';
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatBadgeModule,
+    MatDividerModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatSelectModule,
+    MatStepperModule,
+    MatTabsModule,
+    DragDropModule,
+    MatSnackBarModule,
+    MatSlideToggleModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatCheckboxModule,
+    MatExpansionModule,
+    MatRippleModule
+  ]
+})
+export class AngularModule { }
