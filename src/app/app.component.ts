2bea47b40e928911039e93c5ce632cf4db2b8de6
jonah95419 <jonah95419@gmail.com>
2020-06-24 21:54:41 -0500

    APP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuariosService } from './servicios/service/usuarios.service';
+import { CotizacionService } from './servicios/service/cotizacion.service';
+import { ServiciosAlbanieleriaService } from './servicios/service/servicios_albanieleria.service';
+import { CotizacionDetallesService } from './servicios/service/cotizacion_detalles.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-yo';
+
+  constructor(
+    private serviciosAlbanieleriaApi: ServiciosAlbanieleriaService,
+    private cotizacionApi: CotizacionService,
+    private usuariosApi: UsuariosService,
+    private detallesCotizacionApi: CotizacionDetallesService
+  ){
+    serviciosAlbanieleriaApi.cargarTodo();
+    cotizacionApi.cargarTodo();
+    usuariosApi.cargarTodo();
+    detallesCotizacionApi.cargarTodo();
+  }
+
 }
