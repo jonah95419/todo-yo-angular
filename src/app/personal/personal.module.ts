110a4981753248fedba00b1bf45af72c30d5edc2
jonah95419 <jonah95419@gmail.com>
2020-06-26 17:00:35 -0500

    PERSONAL MODULE

--- src/app/personal/personal.module.ts
+++ src/app/personal/personal.module.ts
@@ -1,13 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PersonalComponent } from './personal/personal.component';
-
+import { PerfilComponent } from './perfil/perfil.component';
+import { AngularModule } from '../angular.module';
+import { AppRoutingModule } from '../app-routing.module';
+import { InformacionComponent } from './informacion/informacion.component';
+import { RendimientoComponent } from './rendimiento/rendimiento.component';
+import { HistorialComponent } from './historial/historial.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { DragDirective } from './informacion/dragDrop.Directive';
+import { RatingModule } from 'ng-starrating';
 
 
 @NgModule({
-  declarations: [PersonalComponent],
+  declarations: [PersonalComponent, PerfilComponent, InformacionComponent, RendimientoComponent, HistorialComponent, DragDirective],
   imports: [
-    CommonModule
+    AppRoutingModule,
+    CommonModule,
+    AngularModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    RatingModule,
+    ReactiveFormsModule.withConfig({warnOnNgModelWithFormControl: 'never'}),
+  ],
+  exports: [
+    AngularModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ]
 })
 export class PersonalModule { }
