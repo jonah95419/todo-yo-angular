ee622d2fa6b306ad077d2e0a42aeb14c7626524d
jonah95419 <jonah95419@gmail.com>
2020-06-26 17:00:26 -0500

    PERSONAL SERVICE

---
+++ src/app/personal/service/interaccion.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList, AngularFireObject, SnapshotAction } from '@angular/fire/database';  // Firebase modules for Database
+import { BehaviorSubject, Observable } from 'rxjs';
+import { personalI } from '../model/personal';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AngularFireStorage } from 'angularfire2/storage';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InteraccionService {
+
+  constructor(private db: AngularFireDatabase, private storage: AngularFireStorage, private _snackBar: MatSnackBar) { }
+
+  cargarTodo = (id: string): Observable<SnapshotAction<any>[]> => {
+    return this.db.list('interaccion/' + id).snapshotChanges().pipe(
+      map((data: any[]) => data.map( item => {
+          let a = item.payload.toJSON();
+          a['key'] = item.key;
+          return a;
+        })
+      )
+    )
+  }
+
+  actualizarEstadoPersonal = (id: string, estado: boolean) => {
+    this.db.object('personal/' + id).update({estado: estado})
+    .then(value => { this.openSnackBar("Registro actualizado", "Ok") })
+    .catch( error => { this.openSnackBar("Ah ocurrido un error", "Ok")})
+  }
+
+
+  private openSnackBar = (message: string, action: string) => {
+    this._snackBar.open(message, action, { duration: 5000 });
+  }
+
+}
---
+++ src/app/personal/service/personal.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';  // Firebase modules for Database
+import { BehaviorSubject } from 'rxjs';
+import { personalI } from '../model/personal';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AngularFireStorage } from 'angularfire2/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonalService {
+
+  private _todos = new BehaviorSubject<personalI[]>([]);
+  private dataStore: { personal: personalI[] } = { personal: [] };
+  readonly todos = this._todos.asObservable();
+
+  constructor(private db: AngularFireDatabase, private storage: AngularFireStorage, private _snackBar: MatSnackBar) { }
+
+  cargarTodo = () => {
+     this.db.list('personal').snapshotChanges().subscribe(
+      (data: any[]) => {
+        this.dataStore.personal = data.map( item => {
+          let a = item.payload.toJSON();
+          a['key'] = item.key;
+          return a;
+        });
+        this._todos.next(Object.assign({}, this.dataStore).personal);
+      }
+    );
+  }
+
+  agregarPersonal = (informacion: any) => {
+    let nuevo: personalI = {
+      nombre: informacion.nombre,
+      cargo: informacion.cargo,
+      mail: '',
+      contacto: '',
+      estado: true,
+      foto: ''
+    }
+    this.db.list('personal').push(nuevo)
+    .then(value => { this.openSnackBar("Registro actualizado", "Ok") })
+    .catch( error => { this.openSnackBar("Ah ocurrido un error", "Ok")})
+  }
+
+  actualizarEstadoPersonal = (id: string, estado: boolean) => {
+    this.db.object('personal/' + id).update({estado: estado})
+    .then(value => { this.openSnackBar("Registro actualizado", "Ok") })
+    .catch( error => { this.openSnackBar("Ah ocurrido un error", "Ok")})
+  }
+
+  actualizarInformacion = (id: String, informacion: any) => {
+    this.db.object('personal/' + id).update(informacion)
+    .then(value => { this.openSnackBar("Registro actualizado", "Ok") })
+    .catch( error => { this.openSnackBar("Ah ocurrido un error", "Ok")})
+  }
+
+  actualizarImagen = (id: String, url: string, url_anterior: string) => {
+    this.db.object('personal/' + id).update({foto: url})
+    .then(value => { this.delete(url_anterior); this.openSnackBar("Registro actualizado", "Ok") })
+    .catch( error => { this.openSnackBar("Ah ocurrido un error", "Ok")})
+  }
+
+  eliminarPersonal = (id: string) => {
+    this.db.object('personal/' + id).remove()
+    .then(value => { this.openSnackBar("Registro eliminado", "Ok") })
+    .catch( error => { this.openSnackBar("Ah ocurrido un error", "Ok")})
+  }
+
+  private delete(downloadUrl) {
+    return this.storage.storage.refFromURL(downloadUrl).delete();
+  }
+
+  private openSnackBar = (message: string, action: string) => {
+    this._snackBar.open(message, action, { duration: 5000 });
+  }
+
+}
