86e1c4fa847bc93e61ffbb2cdd36bad316ca1c04
jonah95419 <jonah95419@gmail.com>
2020-06-29 04:28:16 -0500

    PROMOCIONES MODEL

---
+++ src/app/promociones/model/pomociones.ts
@@ -0,0 +1,11 @@
+export interface promocionesI {
+  descripcion: string;
+  estado: boolean;
+  fecha: string;
+  nombre1: string;
+  subtitulo: string;
+  tipo: string;
+  url: string;
+  key?: string;
+  historial?: boolean;
+}
