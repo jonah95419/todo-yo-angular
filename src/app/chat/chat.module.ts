87450241831a75a642771d385a86516ffa2f3a34
jonah95419 <jonah95419@gmail.com>
2020-06-28 20:51:17 -0500

    CHAT MODULE

---
+++ src/app/chat/chat.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChatComponent, DialogContentExampleDialog } from './chat/chat.component';
+import { AngularModule } from '../angular.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
+
+@NgModule({
+  declarations: [ChatComponent, DialogContentExampleDialog],
+  imports: [
+    CommonModule,
+    AngularModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule.withConfig({warnOnNgModelWithFormControl: 'never'}),
+  ],
+  exports: [
+    ChatComponent,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  entryComponents: [
+    DialogContentExampleDialog
+  ],
+})
+export class ChatModule { }
