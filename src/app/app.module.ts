2a7061f24acaafbe9a2f8062d62282ed75f6583a
jonah95419 <jonah95419@gmail.com>
2020-06-28 20:51:06 -0500

    APP MODULE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,12 @@ import { AngularModule } from './angular.module';
 import { PromocionesModule } from './promociones/promociones.module';
 import { ServiciosModule } from './servicios/servicios.module';
 import { PersonalModule } from './personal/personal.module';
-import { ServiciosAlbanieleriaService } from './servicios/service/servicios_albanieleria.service';
+import { Servicio1Service } from './servicios/service/servicios1.service';
+import { Servicio2Service } from './servicios/service/servicios2.service';
+import { Servicio3Service } from './servicios/service/servicios3.service';
+import { Servicio4Service } from './servicios/service/servicios4.service';
+import { Servicio5Service } from './servicios/service/servicios5.service';
+import { Servicio6Service } from './servicios/service/servicios6.service';
 import { CotizacionService } from './servicios/service/cotizacion.service';
 import { UsuariosService } from './servicios/service/usuarios.service';
 import { CotizacionDetallesService } from './servicios/service/cotizacion_detalles.service';
@@ -28,6 +33,9 @@ import { InteraccionService } from './personal/service/interaccion.service';
 registerLocaleData(localeES, 'es');
 
 // import { RatingModule } from 'ng-starrating';
+import { ChatModule } from './chat/chat.module';
+import { ChatService } from './chat/service/chat.service';
+import { MAT_DATE_LOCALE, DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -44,6 +52,7 @@ registerLocaleData(localeES, 'es');
     PromocionesModule,
     PersonalModule,
     HttpClientModule,
+    ChatModule,
     // RatingModule,
     TranslateModule.forRoot({
       loader: {
@@ -51,23 +60,33 @@ registerLocaleData(localeES, 'es');
           useFactory: HttpLoaderFactory,
           deps: [HttpClient]
       }
-  })
-  ],
+  })],
   providers: [
-    ServiciosAlbanieleriaService,
+    Servicio1Service,
+    Servicio2Service,
+    Servicio3Service,
+    Servicio4Service,
+    Servicio5Service,
+    Servicio6Service,
     CotizacionService,
     CotizacionDetallesService,
     UsuariosService,
     PersonalService,
-    InteraccionService
+    InteraccionService,
+    ChatService,
+    {provide: MAT_DATE_LOCALE, useValue: 'es-EC'}
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(private translate: TranslateService){
+  constructor(private translate: TranslateService, private adapter: DateAdapter<any>){
     translate.addLangs(['es']);
     translate.setDefaultLang('es');
     translate.use('es');
+  }
+
+  setDateAdapterLocale(localeString: string): void {
+    this.adapter.setLocale(localeString);
   }
 }
 
