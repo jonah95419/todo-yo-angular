1b5a850528824e9755f1de14873b4308dc37cbad
jssalazar <jssalazar3@espe.edu.ec>
2020-07-23 15:41:47 -0500

    PERSONAL HISTORIAL

--- src/app/personal/historial/historial.component.ts
+++ src/app/personal/historial/historial.component.ts
@@ -1,21 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { combineLatest, Observable } from 'rxjs';
 import { CotizacionDetallesService } from '../../servicios/service/cotizacion_detalles.service';
 import { CotizacionService } from '../../servicios/service/cotizacion.service';
 import { map, tap } from 'rxjs/operators';
 import { obtenerInteracciones } from '../../utils/obtenerHistorial';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 
 @Component({
   selector: 'personal-historial',
   templateUrl: './historial.component.html',
   styleUrls: ['./historial.component.css']
 })
-export class HistorialComponent implements OnInit {
+export class HistorialComponent implements OnInit, OnDestroy {
 
   historial$: Observable<any[]>;
+  locale: string;
+
+  private _translate;
 
   constructor(
+    private translate: TranslateService,
     private route: ActivatedRoute,
     private cotizacionApi: CotizacionService,
     private cotizacionDetalleApi: CotizacionDetallesService) {
@@ -24,9 +29,20 @@ export class HistorialComponent implements OnInit {
         const id = params.id;
         if(id) { this.obtenerInformacion(id); }
       });
+      translate.setDefaultLang('es');
+      translate.use('es');
    }
 
   ngOnInit(): void {
+    this.locale = this.translate.currentLang;
+    this._translate =  this.translate.onLangChange
+    .subscribe((langChangeEvent: LangChangeEvent) => {this.locale = langChangeEvent.lang;})
+  }
+
+  ngOnDestroy(): void {
+    if(this._translate != undefined) {
+      this._translate.unsubscribe();
+    }
   }
 
   private obtenerInformacion = (id: string): void => {
@@ -34,7 +50,7 @@ export class HistorialComponent implements OnInit {
       this.cotizacionApi.todos,
       this.cotizacionDetalleApi.todos)
     .pipe(
-      map( data => new obtenerInteracciones(id, data[0], data[1]).combinarTablas()),
+      map( data => new obtenerInteracciones(id, data[0], data[1]).combinarTablas())
     );
   }
 
