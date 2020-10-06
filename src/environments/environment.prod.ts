04131dbc02cfebbe00194c486c8112f1a37e4abb
jssalazar <jonah95419@gmail.com>
2020-10-06 12:07:55 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDauoo1M_OSHCxvslV0vvkCN6bOvCyuTbY",
+    authDomain: "todo-yo.firebaseapp.com",
+    databaseURL: "https://todo-yo.firebaseio.com",
+    projectId: "todo-yo",
+    storageBucket: "todo-yo.appspot.com",
+    messagingSenderId: "729605198662",
+    appId: "1:729605198662:web:6dbb79c9402e89ca4d4b47"
+  }
 };
